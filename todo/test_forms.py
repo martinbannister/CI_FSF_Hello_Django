6a49d607bc30d9083627400ad149276a905b4617
Martin Bannister <martin.bannister91@gmail.com>
2022-02-02 20:00:54 +0000

    some changes

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -3,7 +3,7 @@ from .forms import ItemForm
 
 
 class TestItemForm(TestCase):
-    
+
     def test_item_name_is_required(self):
         form = ItemForm({'name': ''})
         self.assertFalse(form.is_valid())
